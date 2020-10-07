b6d1eec43683f64fc5051eb6862d1bfd0fc54ddc
JsonTom888 <Good666888@163.com>
2020-10-07 14:16:46 +0800

    springboot项目

---
+++ src/main/java/com/example/springbootjenkins/controller/JenkinsController.java
@@ -0,0 +1,22 @@
+package com.example.springbootjenkins.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author tom
+ * @version V1.0
+ * @date 2020/10/7 14:12
+ */
+@RestController
+@RequestMapping
+public class JenkinsController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello jenkins";
+    }
+
+
+}
