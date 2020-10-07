e4b0d3cb0f1d6a429051113937ed92b8895c0b27
JsonTom888 <Good666888@163.com>
2020-10-07 17:01:29 +0800

    Jenkins部署脚本

--- Jenkins-in/start.sh
+++ Jenkins-in/start.sh
@@ -1,4 +1,6 @@
 JARFILE=springboot-jenkins-0.0.1-SNAPSHOT.jar
 ps -ef | grep $JARFILE | grep -v grep | awk '{print $2}' | xargs kill -9
 nohup java -jar target/$JARFILE > /mysoft/javalog/springboot.log &
-echo "springboot started successfully"
+if [ $? = 0 ];then
+        echo "springboot started successfully"
+fi
